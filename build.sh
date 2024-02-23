91153827403cc526b77d88bad28f6c879a0ed67d
sttk <sttk.xslet@gmail.com>
2024-02-23 11:04:59 +0900

    chore: added build.sh

---
+++ build.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+errcheck() {
+  exitcd=$1
+  if [[ "$exitcd" != "0" ]]; then
+    exit $exitcd
+  fi
+}
+
+clean() {
+  cargo clean
+  errcheck $?
+}
+
+format() {
+  cargo fmt
+  errcheck $?
+}
+
+compile() {
+  cargo build
+  errcheck $?
+}
+
+test() {
+  cargo test
+  errcheck $?
+}
+
+unit() {
+  cargo test -- $1
+  errcheck $?
+}
+
+cover() {
+  cargo llvm-cov
+  errcheck $?
+}
+
+bench() {
+  cargo bench -- $1
+  errcheck $?
+}
+
+if [[ "$#" == "0" ]]; then
+  clean
+  format
+  compile
+  test
+  cover
+
+elif [[ "$1" == "unit" ]]; then
+  unit $2
+
+else
+  for a in "$@"; do
+    case "$a" in
+    clean)
+      clean
+      ;;
+    format)
+      format
+      ;;
+    compile)
+      compile
+      ;;
+    test)
+      test
+      ;;
+    cover)
+      cover
+      ;;
+    bench)
+      bench
+      ;;
+    '')
+      compile
+      ;;
+    *)
+      echo "Bad task: $a"
+      exit 1
+      ;;
+    esac
+  done
+fi
