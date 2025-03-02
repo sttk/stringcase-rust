2b1f2612a0a5d529a4c426a7bc9a7959eea835bc
Takayuki Sato <sttk.xslet@gmail.com>
2025-03-02 14:57:09 +0900

    test: added more tests for coverage (#34)

--- tests/macro_case_test.rs
+++ tests/macro_case_test.rs
@@ -1,45 +1,84 @@
 use stringcase::{macro_case, macro_case_with_options, Options};
 
+#[allow(deprecated)]
+use stringcase::{macro_case_with_keep, macro_case_with_nums_as_word, macro_case_with_sep};
+
 #[test]
 fn it_should_convert_to_macro_case() {
-    let converted = macro_case("foo-bar100%-baz-qux");
+    let converted = macro_case("foo-bar100%BAZQux");
     assert_eq!(converted, "FOO_BAR100_BAZ_QUX");
 }
 
+#[cfg(test)]
+mod tests_of_macro_case_with_options {
+    use super::*;
+
+    #[test]
+    fn separate_before_non_alphabets() {
+        let opts = Options::new(true, false, "", "%");
+        let converted = macro_case_with_options("foo-bar100%BAZQux", &opts);
+        assert_eq!(converted, "FOO_BAR_100%BAZ_QUX");
+    }
+
+    #[test]
+    fn separate_after_non_alphabets() {
+        let opts = Options::new(false, true, "", "%");
+        let converted = macro_case_with_options("foo-bar100%BAZQux", &opts);
+        assert_eq!(converted, "FOO_BAR100%_BAZ_QUX");
+    }
+
+    #[test]
+    fn separate_before_and_after_non_alphabets() {
+        let opts = Options::new(true, true, "", "%");
+        let converted = macro_case_with_options("foo-bar100%BAZQux", &opts);
+        assert_eq!(converted, "FOO_BAR_100%_BAZ_QUX");
+    }
+
+    #[test]
+    fn not_separate_before_and_after_non_alphabets() {
+        let opts = Options::new(false, false, "", "%");
+        let converted = macro_case_with_options("foo-bar100%BAZQux", &opts);
+        assert_eq!(converted, "FOO_BAR100%BAZ_QUX");
+    }
+
+    #[test]
+    fn specify_separators() {
+        let opts = Options {
+            separators: "-_",
+            ..Default::default()
+        };
+        let converted = macro_case_with_options("foo-bar100%BAZQux", &opts);
+        assert_eq!(converted, "FOO_BAR100%_BAZ_QUX");
+    }
+
+    #[test]
+    fn specify_kept_characters() {
+        let opts = Options {
+            keep: "-$",
+            ..Default::default()
+        };
+        let converted = macro_case_with_options("foo-bar100%BAZQux", &opts);
+        assert_eq!(converted, "FOO-_BAR100_BAZ_QUX");
+    }
+}
+
+#[allow(deprecated)]
 #[test]
 fn it_should_convert_to_macro_case_with_sep() {
-    let opts = Options {
-        separate_before_non_alphabets: false,
-        separate_after_non_alphabets: true,
-        separators: "_",
-        keep: "",
-    };
-    let converted = macro_case_with_options("foo_bar100%BAZQux", &opts);
+    let converted = macro_case_with_sep("foo_bar100%BAZQux", "_");
     assert_eq!(converted, "FOO_BAR100%_BAZ_QUX");
 }
 
+#[allow(deprecated)]
 #[test]
 fn it_should_convert_to_macro_case_with_keep() {
-    let opts = Options {
-        separate_before_non_alphabets: false,
-        separate_after_non_alphabets: true,
-        separators: "",
-        keep: "%",
-    };
-
-    let converted = macro_case_with_options("foo_bar100%BAZQux", &opts);
+    let converted = macro_case_with_keep("foo_bar100%BAZQux", "%");
     assert_eq!(converted, "FOO_BAR100%_BAZ_QUX");
 }
 
+#[allow(deprecated)]
 #[test]
 fn it_should_convert_to_macro_case_with_nums_as_word() {
-    let opts = Options {
-        separate_before_non_alphabets: true,
-        separate_after_non_alphabets: true,
-        separators: "",
-        keep: "",
-    };
-
-    let converted = macro_case_with_options("foo-bar100%-baz-qux", &opts);
+    let converted = macro_case_with_nums_as_word("foo-bar100%-baz-qux");
     assert_eq!(converted, "FOO_BAR_100_BAZ_QUX");
 }
--- tests/pascal_case_test.rs
+++ tests/pascal_case_test.rs
@@ -1,31 +1,77 @@
 use stringcase::{pascal_case, pascal_case_with_options, Options};
 
+#[allow(deprecated)]
+use stringcase::{pascal_case_with_keep, pascal_case_with_sep};
+
 #[test]
 fn it_should_convert_to_pascal_case() {
     let converted = pascal_case("foo_bar100%BAZQux");
     assert_eq!(converted, "FooBar100BazQux");
 }
 
+#[cfg(test)]
+mod tests_of_pascal_case_with_options {
+    use super::*;
+
+    #[test]
+    fn separate_before_non_alphabets() {
+        let opts = Options::new(true, false, "", "%");
+        let converted = pascal_case_with_options("foo_bar100%BAZQux", &opts);
+        assert_eq!(converted, "FooBar100%bazQux");
+    }
+
+    #[test]
+    fn separate_after_non_alphabets() {
+        let opts = Options::new(false, true, "", "%");
+        let converted = pascal_case_with_options("foo_bar100%BAZQux", &opts);
+        assert_eq!(converted, "FooBar100%BazQux");
+    }
+
+    #[test]
+    fn separate_before_and_after_non_alphabets() {
+        let opts = Options::new(true, true, "", "%");
+        let converted = pascal_case_with_options("foo_bar100%BAZQux", &opts);
+        assert_eq!(converted, "FooBar100%BazQux");
+    }
+
+    #[test]
+    fn not_separate_before_and_after_non_alphabets() {
+        let opts = Options::new(false, false, "", "%");
+        let converted = pascal_case_with_options("foo_bar100%BAZQux", &opts);
+        assert_eq!(converted, "FooBar100%bazQux");
+    }
+
+    #[test]
+    fn specify_separators() {
+        let opts = Options {
+            separators: "-_",
+            ..Default::default()
+        };
+        let converted = pascal_case_with_options("foo_bar100%BAZQux", &opts);
+        assert_eq!(converted, "FooBar100%BazQux");
+    }
+
+    #[test]
+    fn specify_kept_characters() {
+        let opts = Options {
+            keep: "_$",
+            ..Default::default()
+        };
+        let converted = pascal_case_with_options("foo_bar100%BAZQux", &opts);
+        assert_eq!(converted, "Foo_Bar100BazQux");
+    }
+}
+
+#[allow(deprecated)]
 #[test]
 fn it_should_convert_to_pascal_case_with_sep() {
-    let opts = Options {
-        separate_before_non_alphabets: false,
-        separate_after_non_alphabets: true,
-        separators: "_",
-        keep: "",
-    };
-    let converted = pascal_case_with_options("foo_bar100%BAZQux", &opts);
+    let converted = pascal_case_with_sep("foo_bar100%BAZQux", "_");
     assert_eq!(converted, "FooBar100%BazQux");
 }
 
+#[allow(deprecated)]
 #[test]
 fn it_should_convert_to_pascal_case_with_keep() {
-    let opts = Options {
-        separate_before_non_alphabets: false,
-        separate_after_non_alphabets: true,
-        separators: "",
-        keep: "%",
-    };
-    let converted = pascal_case_with_options("foo_bar100%BAZQux", &opts);
+    let converted = pascal_case_with_keep("foo_bar100%BAZQux", "%");
     assert_eq!(converted, "FooBar100%BazQux");
 }
